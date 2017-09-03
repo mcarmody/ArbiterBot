d840fcce58737d2342d6719603cb364a636a8b4f
Tim Zwiebel <timzwiebel@gmail.com>
2017-09-02 19:33:23 -0700

    [Dota] Redesign heropoll to allow open and closed polls.
    The heropoll command has been completely redesigned:
     - Hero names now uses heroes.json
     - Starting a poll requires the 'start' subcommand
       - Starting a poll with no hero names creates an open poll
       - Starting a poll with comma-separated hero names creates a closed poll
     - The poll author can no longer vote on their own polls
       - But the poll author can suggest as many times as they want
     - Votes can now be cast either as a number (index), or string (hero name)
    
    There was a somewhat significant amount of refactoring required for these
    changes.
    
    Fixes #2.
    Fixes #4.  (User input is now validated against heroes.json)

--- cogs/dota.py
+++ cogs/dota.py
@@ -15,19 +15,28 @@ class Dota(object):
   async def heropoll(self, ctx):
     """Starts/stops a poll to choose a hero to play.
 
-    Usage:
-      Start a poll:
-        heropoll <comma-separated list of options>
-      Stop a poll and print the results:
-        heropoll stop
-      Stop a poll without printing the results:
-        heropoll abort
+    Polls can be "open" or "closed":
+     - Open polls allow anyone to suggest any hero. In addition, the poll author can make as many suggestions as they like by sending messages with hero names.
+     - Closed polls only allow the hero options they were created with.
 
-    Examples:
-      heropoll Brewmaster, Lone Druid, Meepo
-      heropoll abort
-      heropoll Viper, Hero that gets dumpstered by Viper in lane
-      heropoll stop
+    Only one ongoing poll is allowed per text channel.
+
+    Hero names are not case-sensitive. Nicknames (e.g., AM for Anti-Mage, PL for Phantom Lancer, etc.) may also be used. The bot will do its best to understand suggested hero names.
+
+    If the bot has the "Manage Messages" permission, it will delete vote messages from users (after counting them).
+
+    Usage:
+      Start an open poll:
+        heropoll start
+        Viper
+        Lina
+      Start a closed poll:
+        heropoll start <comma-separated list of hero names>
+        (E.g., heropoll start Brewmaster, Lone Druid, Meepo)
+      Stop the poll and print the results:
+        heropoll stop
+      Stop the poll without printing the results:
+        heropoll abort
     """
     await self._heropoll_command.heropoll(ctx)
 
